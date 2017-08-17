6fd5d5dfc2db50f4e9f4f7111b2c4c69a6e1b700
利竞劲 <137195851@qq.com>
2017-08-17 17:18:07 +0800

    Add files via upload

---
+++ type.js
@@ -0,0 +1,47 @@
+var type = function(label,time_interval,input) {
+    // $(document.body).append('<style>p{display:inline}</style>')
+    var text = input
+    var splitArr=text.split('<br>')
+    var lengthArr=[0]
+    for(var i=0; i<splitArr.length-1; i++){
+        if(i>0){
+            lengthArr[i]=lengthArr[i-1]+splitArr[i].length
+        }else{
+            lengthArr[i]=splitArr[i].length-1
+        }
+    }
+    var arr=[]
+    for(var h=0; h<splitArr.length; h++){
+        var l = splitArr[h].length;　　
+        var s = splitArr[h];　　
+        for (var i = 0; i < l; i++) {
+            arr.push(s.substr(i, 1))
+        }
+    }    　　
+    var t = 0;　　
+    var interval = setInterval(function() {
+        if (t === arr.length) {
+            clearInterval(interval)
+            return
+        }
+        for(var i=0; i< lengthArr.length; i++){
+            if (t ===  lengthArr[i]) {
+                $(label+" i").before('<span>' + arr[t] + '</span><br />');
+                t++;
+                return
+            }
+        }
+        
+        $(label+" i").before('<span>' + arr[t] + '</span>');
+
+        t++;
+    }, time_interval)
+    setInterval(function(){
+        setTimeout(function(){
+            $(label+" i").hide()
+        },100)
+        setTimeout(function(){
+            $(label+" i").show()
+        },700)
+    },800)
+}
